64dba1d8666937150797056143daaa420e9d600d
ariely tamirys <arielytamirys.la@gmail.com>
2022-09-01 10:58:38 -0300

    usando jpa

--- alura/src/main/java/br/com/alura/forum/controllers/TopicosController.java
+++ alura/src/main/java/br/com/alura/forum/controllers/TopicosController.java
@@ -1,24 +1,33 @@
 package br.com.alura.forum.controllers;
 
-import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import br.com.alura.forum.modelo.Curso;
 import br.com.alura.forum.modelo.Topico;
+import br.com.alura.forum.repository.TopicoRepository;
 
 @RestController
 @RequestMapping("/topicos")
 public class TopicosController {
 
+	@Autowired
+	private TopicoRepository topicoRepository;
+
 	@GetMapping
 	public List<TopicoDto> lista() {
-		Topico topico = new Topico("duvida com Spring", "Programação", new Curso("Spring", "Programação"));
+		if (nomeCurso == null) {
+			List<Topico> topicos = topicoRepository.findAll();
+			return TopicoDto.converter(topicos);
+		} else {
+			List<Topico> topicos = topicoRepository.findByTitulo(nomeCurso);
+			return TopicoDto.converter(topicos);
 
-		return TopicoDto.converter(Arrays.asList(topico, topico, topico));
+		}
+
 	}
 
 }
