1fd88da4d11399567c39386fca9f1a38d2e7597a
ariely tamirys <arielytamirys.la@gmail.com>
2022-10-19 14:37:50 -0300

    simplificação de resposta de erro do json

--- alura/src/main/java/br/com/alura/forum/config/validacao/ErroDeFormulario.java
+++ alura/src/main/java/br/com/alura/forum/config/validacao/ErroDeFormulario.java
@@ -5,6 +5,14 @@ public class ErroDeFormulario {
 	private String campo;
 	private String erro;
 	
+	
+	
+	public ErroDeFormulario(String campo, String erro) {
+		this.campo = campo;
+		this.erro = erro;
+	}
+
+
 	public String getCampo() {
 		return campo;
 	}
--- alura/src/main/java/br/com/alura/forum/config/validacao/ErroDeValidacaoHandler.java
+++ alura/src/main/java/br/com/alura/forum/config/validacao/ErroDeValidacaoHandler.java
@@ -3,7 +3,11 @@ package br.com.alura.forum.config.validacao;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.http.HttpStatus;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -12,9 +16,12 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 @RestControllerAdvice
 public class ErroDeValidacaoHandler {
 	
-	//(exceptinon..)esta anotação é usada quando,é para ser chamado quando ouver uma excessao dentro de um controller
+	@Autowired
+	private MessageSource messageSource;
 	//(parametro do excep) dentro do paramentro foi posto o tipo de erro na (exception)  que o metodo precisa tratar se ocorrer
-	// (ResponseStatus) fala qual é o parametro que quero que ele devolva no caso 400
+		// (ResponseStatus) fala qual é o parametro que quero que ele devolva no caso 400
+		
+	//(exceptinon..)esta anotação é usada quando,é para ser chamado quando ouver uma excessao dentro de um controller
 	
 	@ResponseStatus(code =HttpStatus.BAD_REQUEST)
 	@ExceptionHandler(MethodArgumentNotValidException.class)
@@ -22,9 +29,15 @@ public class ErroDeValidacaoHandler {
 		
 		List<ErroDeFormulario> dto = new ArrayList<>();
 		 
+		List<FieldError> fieldErrors = exception.getBindingResult().getFieldErrors();
+		 fieldErrors.forEach(e -> {
+			  String menssagem =messageSource.getMessage(e,LocaleContextHolder.getLocale());
+			  ErroDeFormulario erro = new ErroDeFormulario (e.getField() , menssagem );
+			  dto.add(erro);
+			  
+		 });
+
 		
-		 
-		 
 		 return dto;
 
 
